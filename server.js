0249ff0e232365aaad8b69d4164760987485de5a
8connor <connorh16@gmail.com>
2020-08-16 00:23:43 -0400

    removed some comments.'

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
-// Require all models
+
 var db = require("./models");
 var PORT = 3000;
-// Initialize Express
+
 var app = express();
 
 // Handlebars
@@ -33,8 +30,6 @@ mongoose.connect("mongodb://localhost/scrapedisc", { useNewUrlParser: true });
 // ROUTES
 // =================================================================================
 
-// app.get("/saved", function(req, res) {
-//   res.render("saved")// });
 
 var articles = [];
 
