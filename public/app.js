825ebfcae0b63667195e7e17c37361c77dfb64ae
8connor <connorh16@gmail.com>
2020-08-19 02:19:59 -0400

    added a modal for creating a note.

--- public/app.js
+++ public/app.js
@@ -32,11 +32,27 @@ $(document).on("click", ".savebtn", function () {
 
   if (window.location.pathname === "/saved") {
     artNum.isSaved = false;
-  };
-  
+  }
+
   $.post("/api/saved", artNum, (data) => {
-    $(this).prev().hide()
-    $(this).hide()
+    $(this).prev().hide();
+    $(this).hide();
     console.log(data);
+  });
+});
+
+$(document).on("click", ".noteBtn", function () {
+  console.log();
+
+  var note = {
+    noteNum: $(this).prev().attr("id"),
+    title: $(".noteTitle").val(),
+    body: $(".noteBody").val(),
+  };
+
+  $(".noteSubmit").on("click", function () {
+    $.post("/api/note", note, (data) => {
+      console.log(data);
+    });
   });
 });
